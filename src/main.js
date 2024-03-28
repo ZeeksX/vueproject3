f010758f99db9fdddccadbb741b26450758a97f1
ZeeksX <ikinwotezekiel@gmail.com>
2024-03-28 12:28:16 +0100

    added scss

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import NavBar from './components/NavBar.vue'
 import FullBody from "./components/FullBody.vue"
 import "../public/index.css"
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+
 createApp(FullBody).component(".container")
 createApp(NavBar).component(".navbar")
 createApp(App).mount('#app')
