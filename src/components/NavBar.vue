b0c203a4a0a6d37cf16a4327827948a1b128ed54
ZeeksX <ikinwotezekiel@gmail.com>
2024-03-28 14:08:17 +0100

    changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="navbar">
+  <div class="nav">
     <!--<nav class="navlist">
       <h3>{{ text }}</h3>
       <i class="fa-regular fa-moon"></i>
       <h3 @click="toggleText">{{ mode }}</h3>
     </nav>-->
+    <nav class="navbar">
+      <div class="container-fluid-one">
+        <h3 class="navbar-brand">{{ text }}</h3>
+      </div>
+      <div class="container-fluid-two">
+        <i class="bi bi-moon"></i>
+        <h3 @click="toggleText" class="navbar-brand">{{ mode }}</h3>
+      </div>
+    </nav>
   </div>
 </template>
 
