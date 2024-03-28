ba97425c5173e836996b96bf73cd149760a90c47
ZeeksX <ikinwotezekiel@gmail.com>
2024-03-28 17:21:26 +0100

    changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="nav">
+  <div class="nav" :style="{ backgroundColor: backgroundColor }">
     <nav class="navbar">
       <div class="navbar-fluid-one">
         <h3 class="navbar-item-one">{{ text }}</h3>
@@ -17,18 +17,24 @@ export default {
   name: 'NavBar',
   data() {
     return {
-      mode: "Dark mode",
-      text: "Where in the world?"
+      mode: "Dark Mode",
+      text: "Where in the world?",
+      backgroundColor: "hsl(207, 26%, 17%)" 
     }
   },
   methods: {
     toggleText() {
       if (this.mode === 'Light Mode') {
         this.mode = 'Dark Mode';
+        this.backgroundColor = "hsl(207, 26%, 17%)";
+        document.body.style.backgroundColor = "hsl(207, 26%, 17%)"; 
       } else {
         this.mode = 'Light Mode';
+        this.backgroundColor = "hsl(0, 0%, 98%)";
+        document.body.style.backgroundColor = "hsl(0, 0%, 98%)"; 
       }
     }
   }
 }
 </script>
+
