b40a61ce62bb9ec9e926d0d6d2653c94e196f5e5
ZeeksX <ikinwotezekiel@gmail.com>
2024-03-30 10:42:10 +0100

    changes to fullbody

--- src/components/FullBody.vue
+++ src/components/FullBody.vue
@@ -7,40 +7,56 @@
                     v-model="search">
             </div>
         </form>
-        <select id="region">
+        <select id="region" @change="filterCountry" v-model="search">
             <option disabled selected>Filter by Region</option>
             <option v-for="(continent, index) in Continents" :key="index">{{ continent }}</option>
         </select>
         <div class="countryCard">
-            <div v-for="(country, index) in myJSON" class="card" style="width: 18rem;" :key="index">
-                <img :src="country.flags['svg']" class="card-img-top" alt="country-flag">
+            <div v-for="(country, index) in filteredCountries" class="card" style="width: 18rem;" :key="index">
+                <img :src="country.flags.svg" class="card-img-top" alt="country-flag">
                 <div class="card-body">
-                    <h1 class="card-text">{{ country.name }}</h1>
+                    <h1 class="card-text">{{ country.name.common }}</h1>
                     <p>Population: {{ country.population }}</p>
-                    <p>Region: {{ country.region }} </p>
+                    <p>Region: {{ country.region }}</p>
                     <p>Capital: {{ country.capital }}</p>
                 </div>
             </div>
-        </div>    
+        </div>
     </div>
-
 </template>
+
 <script>
-import json from "../assets/data.json";
 export default {
     name: "FullBody",
     data() {
         return {
             text: "Search for a country...",
-            search: "",
-            Continents: ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antartica"],
-            myJSON: json
+            search: "", // Set search to empty initially
+            Continents: ["Filter by Region", "Africa", "Asia", "Europe", "Americas", "Oceania", "Antarctica"],
+            countriesData: [],
+            filteredCountries: []
         };
     },
-    methods: {
-        filterCountry() {
-
-        }
+    mounted() {
+        // Trigger filterCountry on mount to apply default filter
+        this.filterCountry();
     },
+    methods: {
+        async filterCountry() {
+            await this.getData();
+            this.filteredCountries = this.countriesData.filter(country => {
+                const searchMatch = country.name.common.toLowerCase().includes(this.search.toLowerCase());
+                const regionMatch = country.region.toLowerCase().includes(this.search.toLowerCase());
+                return searchMatch || regionMatch;
+            });
+        },
+        async getData() {
+            if (this.countriesData.length === 0) {
+                const response = await fetch("https://restcountries.com/v3.1/all");
+                this.countriesData = await response.json();
+            }
+            this.filteredCountries = this.countriesData; // Initialize filteredCountries
+        }
+    }
 };
 </script>
