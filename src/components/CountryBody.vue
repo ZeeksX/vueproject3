dd41351a5d3cc21a3d64e62d78c8dbb432dea204
ZeeksX <ikinwotezekiel@gmail.com>
2024-04-13 11:29:40 +0100

    added FilterBody

--- src/components/CountryBody.vue
+++ src/components/CountryBody.vue
@@ -1,46 +1,22 @@
 <template>
   <div class="container">
-    <div id="filters">
-      <form id="form" @submit.prevent="filterCountry">
-        <label for="input-field" class="sr-only">Search for a country</label>
-        <div id="input-bar">
-          <i id="search-icon" class="bi bi-search"></i>
-          <input @input="updateCountry" name="searchbar" id="input-field" type="text"
-            placeholder="Search for a country..." aria-label="Search for a country" v-model="search" />
-        </div>
-      </form>
-      <label for="region" class="sr-only">Filter by Region</label>
-      <select id="region" @change="updateRegion" v-model="region" aria-label="Filter by Region">
-        <option disabled selected hidden>Filter by Region</option>
-        <option v-for="(continent, index) in Continents" :key="index">
-          {{ continent }}
-        </option>
-      </select>
-    </div>
+    <FilterBody @filter="updateFilter" />
     <CardBody :filteredCountries="filteredCountries" />
   </div>
 </template>
 
 <script>
 import CardBody from './CardBody.vue';
+import FilterBody from './FilterBody.vue';
 
 export default {
   name: "CountryBody",
   components: {
-    CardBody
+    CardBody,
+    FilterBody,
   },
   data() {
     return {
-      search: "",
-      region: "Filter by Region",
-      Continents: [
-        "Africa",
-        "Americas",
-        "Asia",
-        "Europe",
-        "Oceania",
-        "Antarctic",
-      ],
       countriesData: [],
       filteredCountries: [],
     };
@@ -54,23 +30,15 @@ export default {
         const response = await fetch("https://restcountries.com/v3.1/all");
         this.countriesData = await response.json();
       }
-      this.filterCountry();
+      this.updateFilter();
     },
-    async filterCountry() {
+    updateFilter({ search = "", region = "Filter by Region" } = {}) {
       this.filteredCountries = this.countriesData.filter((country) => {
-        const searchMatch = country.name.common.toLowerCase().includes(this.search.toLowerCase());
-        const regionMatch = country.region.toLowerCase().includes(this.region.toLowerCase());
+        const searchMatch = country.name.common.toLowerCase().includes(search.toLowerCase());
+        const regionMatch = country.region.toLowerCase().includes(region.toLowerCase());
         return searchMatch || regionMatch;
       });
     },
-    updateRegion() {
-      this.search = this.region;
-      this.filterCountry();
-    },
-    updateCountry() {
-      this.region = "Filter by Region";
-      this.filterCountry();
-    },
-  }
-}
+  },
+};
 </script>
