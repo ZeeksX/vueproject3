1e3b6541290a87ec64d911020696e6bd31231277
ZeeksX <ikinwotezekiel@gmail.com>
2024-04-25 18:10:31 +0100

    added router and pinia store

---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home-page">
+        <NavBar />
+        <CountryBody />
+    </div>
+</template>
+<script>
+import CountryBody from "../components/CountryBody.vue";
+import NavBar from "../components/NavBar.vue";
+export default {
+    name: "HomePage",
+    components: {
+        NavBar,
+        CountryBody
+    }
+}
+</script>
